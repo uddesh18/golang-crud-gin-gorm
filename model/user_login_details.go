dab7aaf47f943df6398990e0f72747bd337b8046
Uddesh Takpere <33000178@Heytorus.com>
2024-05-06 12:00:00 +0530

    regid

--- model/user_login_details.go
+++ model/user_login_details.go
@@ -3,10 +3,11 @@ package model
 import "time"
 
 type UserLoginDetails struct {
-	Id          int       `gorm:"type:int;primary_key"`
-	Username    string    `gorm:"size:255;not null;unique" json:"username"`
-	Password    string    `gorm:"size:255;not null;" json:"password"`
-	AccessToken string    `gorm:"size:5000;not null;" json:"access_token"`
-	CreatedDate time.Time `gorm:"not null;default:CURRENT_TIMESTAMP" json:"created_date"`
-	IsActive    bool      `gorm:"type:bool" json:"is_active"`
+	Id           int       `gorm:"type:int;primary_key"`
+	Username     string    `gorm:"size:255;not null;unique" json:"username"`
+	Password     string    `gorm:"size:255;not null;" json:"password"`
+	AccessToken  string    `gorm:"size:5000;not null;" json:"access_token"`
+	CreatedDate  time.Time `gorm:"not null;default:CURRENT_TIMESTAMP" json:"created_date"`
+	IsActive     bool      `gorm:"type:bool" json:"is_active"`
+	RegisteredId int       `gorm:"type:int" json:"registered_id"`
 }
